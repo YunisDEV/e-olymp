3ee4c877e61724e00e6f7199b3d5c5861590bd1e
Sevil2005 <sevillerasulova@gmail.com>
2021-03-13 15:08:11 +0400

    1024 cpp 5133 cpp

---
+++ 5000-5999/5133 - abc/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    for (int i = 1; i < 4; i++) {
+        if (i==1){
+            cout << "a" << endl;
+        }
+        else if (i==2){
+            cout << "bb" << endl;
+        }
+        else{
+            cout << "ccc" << endl;
+        }
+    }
+    return 0;
+}
