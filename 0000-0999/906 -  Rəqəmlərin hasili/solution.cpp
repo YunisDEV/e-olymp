62a5a87cb036f5d1432cc2aa628db122ad53b93a
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-03-13 15:28:00 +0400

    q906 cpp q959 cpp

---
+++ 0000-0999/906 -  Rəqəmlərin hasili/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int number;
+  int result = 1;
+  cin >> number;
+  while(number > 0){
+    result *= number%10;
+    number /= 10;
+  }
+  cout << result;
+  return 0;
+}
